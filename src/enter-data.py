481c06985b01c3cb001756c7a4bd22b8ee4eac35
Leif <leifl@kth.se>
2024-12-12 11:49:09 +0100

    wrong psycopg syntax

--- src/enter-data.py
+++ src/enter-data.py
@@ -1,30 +1,44 @@
 import os
 import psycopg
 
-def fill_table(tbl_name, connection, insert_row):
+def fill_table(tbl_name, cursor, insert_row):
     with open('datafiles/' + tbl_name + '.txt', 'r') as data_file:
         for line in data_file:
             if len(line) > 1:
-                insert_row(connection, line)
+                insert_row(cursor, line)
     print('inserted ' + tbl_name)
 
 with psycopg.connect("dbname=birddb user=postgres host=localhost password=postgres") as connection:
-    print('connected')
+    with connection.cursor() as cursor:
+        print('connected')
 
-    def insert_location_row(connection, line):
-        connection.execute('INSERT INTO location ("name") VALUES (\'' + line[0:-1].strip() + '\')')
-    fill_table('location', connection, insert_location_row)
+        def insert_region_row(cursor, line):
+            cursor.execute("""INSERT INTO category ("name") VALUES (%s)""", (line[0:-1],))
+        fill_table('region', cursor, insert_region_row)
 
-    def insert_category_row(connection, line):
-        connection.execute('INSERT INTO category ("name") VALUES (\'' + line[0:-1] + '\')')
-    fill_table('category', connection, insert_category_row)
+        def insert_location_row(cursor, line):
+            tokens = line.split(';')
+            location = tokens[0]
+            region = tokens[1][0:-1]
+            print(region)
+            cursor.execute("""INSERT INTO location ("name", "region_id") VALUES (%s, 
+                              (select region_id from region where "name" = %s))""", 
+                              (location, region,))
+        fill_table('location', cursor, insert_location_row)
 
-    def insert_species_row(connection, line):
-        tokens = line.split(';')
-        species = tokens[0]
-        category = tokens[1][0:-1]
-        connection.execute('INSERT INTO species ("name", "category_id") VALUES (\'' + species + 
-                         '\', (select category_id from category where "name" = \'' + category + '\'))')
-    fill_table('species', connection, insert_species_row)
+        def insert_category_row(cursor, line):
+            cursor.execute("""INSERT INTO category ("name") VALUES (%s)""", (line[0:-1],))
+        fill_table('category', cursor, insert_category_row)
 
-    print('done inserting')
+        def insert_species_row(cursor, line):
+            tokens = line.split(';')
+            species = tokens[0]
+            category = tokens[1][0:-1]
+            cursor.execute("""INSERT INTO species ("name", "category_id") VALUES (%s, 
+                              (select category_id from category where "name" = %s))""",
+                              (species, category,))
+        fill_table('species', cursor, insert_species_row)
+
+    connection.commit()
+
+print('done inserting')
