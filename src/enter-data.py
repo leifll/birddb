73a70a1774e1fbc6ef80045e50176b389d0c3a15
Leif <leifl@kth.se>
2025-01-03 17:22:06 +0100

    added photo and obs

--- src/enter-data.py
+++ src/enter-data.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+from datetime import datetime
 from typing import Callable, Any
 from psycopg import connect
 from pydantic import ValidationError, validate_call
@@ -62,15 +62,48 @@ try:
 
             @validate_call
             def insert_book_row(cursor: Any, line: str):
-                date = datetime.datetime.strptime(line[0:-1], '%Y-%m-%d').date()
+                date = datetime.strptime(line[0:-1], '%Y-%m-%d').date()
                 cursor.execute("""INSERT INTO book ("date") VALUES (%s)""", (date,))
             fill_table('book', cursor, insert_book_row)
 
             @validate_call
             def insert_observation_type__row(cursor: Any, line: str):
-                cursor.execute("""INSERT INTO observation_type ("observation_type") VALUES (%s)""", 
+                cursor.execute("""INSERT INTO observation_type ("name") VALUES (%s)""", 
                                (line[0:-1],))
             fill_table('observation_type', cursor, insert_observation_type__row)
+
+            @validate_call
+            def insert_observation_and_photo_rows(cursor: Any, line: str):
+                tokens = line.split(';')
+                dir_name = tokens[0]
+                location = tokens[1].lower()
+                date = None
+                if (len(tokens[2]) > 0):
+                    date = datetime.strptime(tokens[2], '%y%m%d').date()
+                file_name = tokens[3]
+                species = tokens[4].lower()
+                observation_type = tokens[5].lower()
+                quantity = tokens[6]
+                page_no = tokens[7]
+                comment = tokens[8][0:-1]
+
+                cursor.execute("""SELECT value FROM config WHERE name = %s""", ("photo-path-prefix",))
+                file_path_prefix = cursor.fetchone()[0]
+
+                if (len(dir_name) > 0):
+                    cursor.execute("""INSERT INTO photo (file_path, time) VALUES (%s, %s)""", 
+                                       (file_path_prefix + "/" + dir_name + "/" + file_name, date))
+
+                cursor.execute("""INSERT INTO observation (date, location_id, species_id, photo_id,
+                                  observation_type_id, quantity, comment) VALUES (%s,
+                                  (SELECT location_id FROM location WHERE lower(name) = %s),
+                                  (SELECT species_id FROM species WHERE lower(name) = %s),
+                                  (SELECT photo_id FROM photo WHERE file_path ~ %s), 
+                                  (SELECT observation_type_id FROM observation_type WHERE lower(name) = %s),
+                                  NULLIF(%s, '')::int, NULLIF(%s, ''))""", 
+                                  (date, location, species, ".*" + dir_name + "/" + file_name + "$", 
+                                   observation_type, quantity, comment))
+            fill_table('obs_and_photo', cursor, insert_observation_and_photo_rows)
 
     connection.close()
 
